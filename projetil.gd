12604538d2ecba244d071ac0cc180605f55d71ee
Renato de Oliveira <renato819301@gmail.com>
2026-05-10 19:32:34 -0300

    Projeto inicial

---
+++ projetil.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@export var velocidade_base: float = 500.0
+var direcao: Vector2 = Vector2.ZERO
+var velocidade_adicional: Vector2 = Vector2.ZERO
+
+func _process(delta: float) -> void:
+	var movimento_final = (direcao * velocidade_base) + velocidade_adicional
+	global_position += movimento_final * delta
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
