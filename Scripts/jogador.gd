6278b378ec7c9c3dd31fc25a6400b93a70c94f18
Renato de Oliveira <renato819301@gmail.com>
2026-05-18 22:28:13 -0300

    Reorganização de arquivos

---
+++ Scripts/jogador.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+@export var velocidade: float = 300.0
+@export var cena_projetil: PackedScene
+
+@onready var ponto_tiro = $PontoTiro
+@onready var timer_tiro = $TimerTiro
+
+func _physics_process(_delta: float) -> void:
+	# 1. Movimentação
+	var direcao_mov = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direcao_mov * velocidade
+	move_and_slide()
+
+	# 2. Lógica de Tiro
+	var direcao_tiro = Input.get_vector("shoot_left", "shoot_right", "shoot_up", "shoot_down")
+	
+	if direcao_tiro != Vector2.ZERO and timer_tiro.is_stopped():
+		atirar(direcao_tiro)
+
+func atirar(dir: Vector2) -> void:
+	if cena_projetil == null: return
+		
+	var bala = cena_projetil.instantiate()
+	
+	var direcao_final = Vector2.ZERO
+	if abs(dir.x) > abs(dir.y):
+		direcao_final = Vector2(sign(dir.x), 0)
+	else:
+		direcao_final = Vector2(0, sign(dir.y))
+		
+	bala.direcao = direcao_final
+	bala.global_position = ponto_tiro.global_position
+	
+	
+	get_parent().add_child(bala)
+	
+	timer_tiro.start()
